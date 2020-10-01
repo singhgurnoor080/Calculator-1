d82588dab31ccc8818b6b842da0dcf68d56bf16d
singhgurnoor080 <44643783+singhgurnoor080@users.noreply.github.com>
2020-10-02 03:31:04 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,8 +2,8 @@
 
 print("Welcome to the simple python calculator")
 print("What is your name: ")
-user_Name = input(">>")
-print("Hello " + user_Name)
+user_Name = input("  ")
+print("Welcome " + user_Name)
 
 def add(x, y):
     return x+y
@@ -29,7 +29,7 @@ print("1.Addition")
 print("2.Subtraction")
 print("3.Multiplication")
 print("4.Division")
-print("5.Reminder")
+print("5.Remainder")
 print("6.Percentage")
 
 
@@ -44,32 +44,32 @@ print("You entered " +str(num1)+ " as the first number and "+str(num2)+ " as the
 
 #Addition 
 if user_Choice == "1":
-    print("Adding your numbers......")
+    print("Adding your numbers")
     print("The sum of",num1, "+", num2, "is equal to",add(num1, num2))
 
 #Subtraction
 elif user_Choice == "2":
-    print("Ok subtracting your numbers.....")
+    print("subtracting your numbers")
     print("The result of", num1, "-", num2, "is equal to",subtraction(num1, num2))
 
 #Multiplication
 elif user_Choice == "3":
-    print("Multiplying your numbers.....")
+    print("Multiplying your numbers")
     print("Multiplying", num1, "by", num2, "is equal to",multiplication(num1, num2))
 
 #Division
 elif user_Choice == "4":
-    print("Dividing your numbers.....")
+    print("Dividing your numbers")
     print("Dividing", num1, "with", num2, "is equal to",division(num1, num2))
 
 #Modulus/Reminder
 elif user_Choice == "5":
-    print("Getting the reminder for you.....")
+    print("Getting the reminder for you")
     print("Modulus", num1, "after dividing by", num2, "is equal to",modulus(num1, num2))
 
 #Percentage
 elif user_Choice == "6":
-    print("Calculating your percentage.....")
+    print("Calculating your percentage")
     print(num1, "percent of", num2, "is equal to",percent(num1, num2),"%")
 
 else:
